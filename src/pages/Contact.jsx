1408bcec99a5a9ebe53509ef0c3da0ce04aeb22f
Sardorbek <sardorbekbahramov04@gmail.com>
2024-02-14 17:23:42 +0500

    added some features

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Form from '../components/contactpage/formpart/form'
-import Contactinfo from '../components/contactpage/contactinfo/contactinfo'
 
 export default function Contact() {
   return (
     <div>
         <Form/>
-        <Contactinfo/>
     </div>
   )
 }
